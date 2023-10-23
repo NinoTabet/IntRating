ed75fb9bd1f404ccfdc027946dc9e83fd52b90ef
Nino <anthonydavidtabet@gmail.com>
2023-10-23 17:04:26 +0300

    database logic heroku version

--- server/db.js
+++ server/db.js
@@ -1,12 +1,27 @@
-const Pool = require("pg").Pool;
+const { Pool } = require("pg");
 
-const pool = new Pool({
+let pool;
 
-    user: "postgres",
-    password: "password",
-    host: "localhost",
+if (process.env.NODE_ENV === "production") {
+  // Use the Heroku Postgres URL
+  pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  });
+} else {
+  // Use your local database configuration for development
+  pool = new Pool({
+    user: "jtxlohajbynwlq",
+    password: "24d4675f3cd90bcb79a0bde0c8b45a94e5eaf7db112e7771520b1fbb66597bbe",
+    host: "ec2-34-236-103-63.compute-1.amazonaws.com",
     port: 5432,
-    database: "intrating"
-})
+    database: "d6eoj01m6j0ugd",
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  });
+}
 
 module.exports = pool;
