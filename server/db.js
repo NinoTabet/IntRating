11b96963d70d5d714dde531e15bad480f0b40c60
Nino <anthonydavidtabet@gmail.com>
2023-10-26 12:41:46 +0300

    attempted fix of db/favicon

--- server/db.js
+++ server/db.js
@@ -13,11 +13,11 @@ if (process.env.NODE_ENV === "production") {
 } else {
   // Use your local database configuration for development
   pool = new Pool({
-    user: "jtxlohajbynwlq",
-    password: "24d4675f3cd90bcb79a0bde0c8b45a94e5eaf7db112e7771520b1fbb66597bbe",
-    host: "ec2-34-236-103-63.compute-1.amazonaws.com",
+    user: "qhpwtusxskcfng",
+    password: "49dacc3b9a1acc7f1f38ca56f2406718e8ad36d27c9832676bddb996d7a7b5e9",
+    host: "ec2-54-84-182-168.compute-1.amazonaws.com",
     port: 5432,
-    database: "d6eoj01m6j0ugd",
+    database: "d4pmerm1s710f8",
     ssl: {
       rejectUnauthorized: false,
     },
