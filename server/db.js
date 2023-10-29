91e6ea03bf0796fe8ea069cec4a1e293af2bd466
Nino <anthonydavidtabet@gmail.com>
2023-10-30 00:06:54 +0200

    Test fix for db connectivity
    testing fix for database connectivity issue

--- server/db.js
+++ server/db.js
@@ -3,8 +3,8 @@ const { Pool } = require("pg");
 let pool;
 
 pool = new Pool({
-  connectionString: process.env.DATABASE_URL//,
-  //ssl: {rejectUnauthorized: false,}
+  connectionString: process.env.DATABASE_URL
+  ,ssl: {rejectUnauthorized: false}
 });
 
 module.exports = pool;
