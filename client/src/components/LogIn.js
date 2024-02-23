d8c1e1666542d5e08dad1be306a55c1bb88550a1
Nino <anthonydavidtabet@gmail.com>
2024-02-23 23:11:50 +0700

    added react router dom
    react router dom

--- client/src/components/LogIn.js
+++ client/src/components/LogIn.js
@@ -1,62 +1,73 @@
-//LoginSignup
 import React, { useState } from 'react';
 import Cookies from 'js-cookie';
+import { useNavigate } from 'react-router-dom';
 
 const apiUrl = process.env.REACT_APP_API_URL;
 
 const LogIn = ({ setCurrentPage }) => {
-  console.log('API URL:', apiUrl);
-  const [email_address, setEmail_address] = useState("");
-  const [password, setPassword] = useState("");
+  const [email_address, setEmail_address] = useState('');
+  const [password, setPassword] = useState('');
+  const navigate = useNavigate(); // Use useNavigate hook to access the navigate function
 
   const onSubmitForm = async (e) => {
-      e.preventDefault();
-      try {
-
-          const body = { email_address, password }
-          const response = await fetch(apiUrl + "/login", {
-              method: "POST",
-              headers: { "Content-Type": "application/json" },
-              body: JSON.stringify(body)
-          });
-          if (response.ok) {
-              const { token } = await response.json();
-              Cookies.set("jwt_authorization", token, {
-                expires: new Date (token.exp * 1000)
-              });
-              alert("You are now logged in!");
-              setCurrentPage("home");
-          } else {
-              alert("Login failed. Please try again.");
-          }
-      } catch (err) {
-          console.error(err.message);
+    e.preventDefault();
+    try {
+      const body = { email_address, password };
+      const response = await fetch(apiUrl + '/login', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(body),
+      });
+      if (response.ok) {
+        const { token } = await response.json();
+        Cookies.set('jwt_authorization', token, {
+          expires: new Date(token.exp * 1000),
+        });
+        alert('You are now logged in!');
+        navigate('/'); // Redirect to the home page
+      } else {
+        alert('Login failed. Please try again.');
       }
+    } catch (err) {
+      console.error(err.message);
+    }
   };
 
   return (
-    <>
-      <div className="container mx-auto w-100vw d-flex align-items-center justify-content-center" style={{ height: '75vh' }}>
-        <div className="w-50">
-        <h1 className='text-center text-dark'>Login</h1>
-        <div className='text-center mt-5'>
+    <div className="container mx-auto w-100vw d-flex align-items-center justify-content-center" style={{ height: '75vh' }}>
+      <div className="w-50">
+        <h1 className="text-center text-dark">Login</h1>
+        <div className="text-center mt-5">
           <form onSubmit={onSubmitForm}>
             <div className="form-group col-12 col-md-7 mx-auto">
               <label htmlFor="email">Email address</label>
-              <input type="email" className="form-control text-center" id="email" placeholder="Enter your email" value={email_address}
-              onChange={(e) => setEmail_address(e.target.value)}/>
+              <input
+                type="email"
+                className="form-control text-center"
+                id="email"
+                placeholder="Enter your email"
+                value={email_address}
+                onChange={(e) => setEmail_address(e.target.value)}
+              />
             </div>
             <div className="form-group col-12 col-md-7 mx-auto mt-3">
               <label htmlFor="password">Password</label>
-              <input type="password" className="form-control text-center" id="password" placeholder="Enter your password" value={password}
-              onChange={(e) => setPassword(e.target.value)}/>
+              <input
+                type="password"
+                className="form-control text-center"
+                id="password"
+                placeholder="Enter your password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
             </div>
-            <button type="submit" className="btn btn-dark mt-5">Login</button>
+            <button type="submit" className="btn btn-dark mt-5">
+              Login
+            </button>
           </form>
         </div>
-        </div>
       </div>
-    </>
+    </div>
   );
 };
 
--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,123 +1,118 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { NavLink, useLocation } from 'react-router-dom';
+import { createBrowserHistory } from 'history'; // Import the createBrowserHistory function
 import SearchBar from './SearchBar';
 import Cookies from 'js-cookie';
 
-const NavBar = ({ currentPage, setCurrentPage, handleSearchSuccess }) => {
-  const isLoggedIn = Cookies.get('jwt_authorization') !== undefined;
+const NavBar = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const location = useLocation();
+  const history = createBrowserHistory(); // Create a history object
 
-  const handleNavigation = (page) => {
-    console.log('Changing page to', page);
-    if (page === 'logout') {
-      try {
-        Cookies.remove('jwt_authorization');
-        alert("You've been logged out.");
-        setCurrentPage('home');
-      } catch (err) {
-        console.error(err.message);
-      }
-    } else {
-      console.log('Changing page to', page);
-      setCurrentPage(page);
-    }
+  useEffect(() => {
+    // Check login status each time location changes and when component mounts
+    const checkLoginStatus = () => {
+      const jwtToken = Cookies.get('jwt_authorization');
+      setIsLoggedIn(jwtToken !== undefined);
+    };
+
+    checkLoginStatus();
+  }, [location.pathname]); // Re-run effect when location changes
+
+  const handleLogout = () => {
+    // Remove JWT token from cookie
+    Cookies.remove('jwt_authorization');
+    // Update login status
+    setIsLoggedIn(false);
+    // Redirect user to the homepage
+    history.push('/');
   };
 
   return (
-    <>
-      <nav className="navbar navbar-dark justify-content-between mb-5">
-        <div className="container-fluid">
-          <button
-            className="navbar-toggler collapsed"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarNav"
-            aria-controls="navbarNav"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="navbar-collapse collapse" id="navbarNav">
-            <ul className="navbar-nav">
+    <nav className="navbar navbar-dark justify-content-between mb-5">
+      <div className="container-fluid">
+        <button
+          className="navbar-toggler collapsed"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="navbar-collapse collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <NavLink
+                to="/"
+                className="nav-link px-2 text-light"
+                activeClassName="link-secondary"
+                exact
+              >
+                Home
+              </NavLink>
+            </li>
+            {!isLoggedIn && (
               <li className="nav-item">
-                <a
-                  href="#"
-                  className={`nav-link px-2 text-light ${
-                    currentPage === 'home' ? 'link-secondary' : ''
-                  }`}
-                  onClick={() => handleNavigation('home')}
+                <NavLink
+                  to="/loginsignup"
+                  className="nav-link px-2 text-light"
+                  activeClassName="link-secondary"
                 >
-                  Home
-                </a>
+                  Sign up & Log in 
+                </NavLink>
               </li>
-              {!isLoggedIn && (
+            )}
+            <li className="nav-item">
+              <NavLink
+                to="/contact"
+                className="nav-link px-2 text-light"
+                activeClassName="link-secondary"
+              >
+                Feedback
+              </NavLink>
+            </li>
+            {isLoggedIn && (
+              <>
                 <li className="nav-item">
-                  <a
-                    href="#"
-                    className={`nav-link px-2 text-light ${
-                      currentPage === 'loginsignup' ? 'link-secondary' : ''
-                    }`}
-                    onClick={() => handleNavigation('loginsignup')}
+                  <NavLink
+                    to="/contribute"
+                    className="nav-link px-2 text-light"
+                    activeClassName="link-secondary"
                   >
-                    Sign up & Log in 
-                  </a>
+                    Contribute rating
+                  </NavLink>
                 </li>
-              )}
-              <li className="nav-item">
-                <a
-                  href="#"
-                  className={`nav-link px-2 text-light ${
-                    currentPage === 'feedback' ? 'link-secondary' : ''
-                  }`}
-                  onClick={() => handleNavigation('contact')}
-                >
-                  Feedback
-                </a>
-              </li>
-              {isLoggedIn && (
-                <>
-                  <li className="nav-item">
-                    <a
-                      href="#"
-                      className={`nav-link px-2 text-light ${
-                        currentPage === 'contribute' ? 'link-secondary' : ''
-                      }`}
-                      onClick={() => handleNavigation('contribute')}
-                    >
-                      Contribute rating
-                    </a>
-                  </li>
-                  <li className="nav-item">
-                    <a
-                      href="#"
-                      className={`nav-link px-2 text-light ${
-                        currentPage === 'profile' ? 'link-secondary' : ''
-                      }`}
-                      onClick={() => handleNavigation('profile')}
-                    >
-                      Profile
-                    </a>
-                  </li>
-                  <li className="nav-item">
-                    <a
-                      href="#"
-                      className={`nav-link px-2 text-light ${
-                        currentPage === 'logout' ? 'link-secondary' : ''
-                      }`}
-                      onClick={() => handleNavigation('logout')}
-                    >
-                      LogOut
-                    </a>
-                  </li>
-                </>
-              )}
-            </ul>
-          </div>
-          <div className="ms-auto">
-            <SearchBar handleSearchSuccess={handleSearchSuccess} />
-          </div>
+                <li className="nav-item">
+                  <NavLink
+                    to="/profile"
+                    className="nav-link px-2 text-light"
+                    activeClassName="link-secondary"
+                  >
+                    Profile
+                  </NavLink>
+                </li>
+                <li className="nav-item">
+                  <NavLink
+                    to="/"
+                    className="nav-link px-2 text-light"
+                    activeClassName="link-secondary"
+                    onClick={handleLogout} // Handle logout onClick
+                  >
+                    LogOut
+                  </NavLink>
+                </li>
+              </>
+            )}
+          </ul>
         </div>
-      </nav>
-    </>
+        <div className="ms-auto">
+          <SearchBar />
+        </div>
+      </div>
+    </nav>
   );
 };
 
