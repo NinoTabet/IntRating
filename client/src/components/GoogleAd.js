d3d9ef96cdef768a1a39eca226322c12357541b0
Nino <anthonydavidtabet@gmail.com>
2024-10-22 22:27:28 +0700

    g-ads edit 5

--- client/src/components/GoogleAd.js
+++ client/src/components/GoogleAd.js
@@ -1,20 +1,30 @@
-import React from 'react'
-import { Adsense } from '@ctrl/react-adsense';
+import React, { useEffect } from 'react';
 
 function GoogleAd() {
-    console.log("Ads Shown")
-    return (
-        <Adsense
-            className='border border-primary position-fixed top-50 h-25 w-25'
-            client="ca-pub-3366618965875818"
-            slot="7071737081"
-            // adtest='on' //Dev Only
-        />
-    )
+    useEffect(() => {
+        try {
+            (window.adsbygoogle = window.adsbygoogle || []).push({});
+        } catch (e) {
+            console.error("AdSense error:", e);
+        }
+    }, []);
 
+    return (
+        <ins
+        className="border border-primary position-fixed top-50 h-25 w-25"
+        style={{ display: 'block' }}
+        data-ad-client="ca-pub-3366618965875818"
+        data-ad-slot="7071737081"
+        data-ad-format="auto"
+        data-full-width-responsive="true"
+        data-adbreak-test="on">
+        </ins>
+    );
 }
 
 export default GoogleAd;
+
+
 
 /*
 <script async src="https://pageAd2.googlesyndication.com/pageAd/js/Adsbygoogle.js?client=ca-pub-3366618965875818"
